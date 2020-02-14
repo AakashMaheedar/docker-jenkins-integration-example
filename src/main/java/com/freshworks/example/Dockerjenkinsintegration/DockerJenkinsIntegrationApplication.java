e4457263fb700b3ae02851b1efec4d65c955975e
Aakash Maheedar <58288773+Aakash-Maheedar@users.noreply.github.com>
2020-02-14 15:39:20 +0530

    second commit

--- src/main/java/com/freshworks/example/Dockerjenkinsintegration/DockerJenkinsIntegrationApplication.java
+++ src/main/java/com/freshworks/example/Dockerjenkinsintegration/DockerJenkinsIntegrationApplication.java
@@ -1,13 +1,18 @@
 package com.freshworks.example.Dockerjenkinsintegration;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DockerJenkinsIntegrationApplication {
+public class DockerJenkinsIntegrationApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DockerJenkinsIntegrationApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello world!!");
+	}
 }
