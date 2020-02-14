52eb85ffee1bef4974a5d92d09456b24887af014
Aakash Maheedar <58288773+Aakash-Maheedar@users.noreply.github.com>
2020-02-14 15:13:27 +0530

    first commit

---
+++ src/test/java/com/freshworks/example/Dockerjenkinsintegration/DockerJenkinsIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freshworks.example.Dockerjenkinsintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinsIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
